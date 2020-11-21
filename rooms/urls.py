c16241c154549004965877ece54d897a5c497fb4
miranaky <miranaky@gmail.com>
2020-11-21 10:34:39 -0600

    Django REST API - CRUD room list with viewsets

--- rooms/urls.py
+++ rooms/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from . import views
+from rest_framework.routers import DefaultRouter
+from . import viewsets
 
 app_name = "rooms"
 
-urlpatterns = [
-    path("list/", views.ListRoomsView.as_view()),
-    path("<int:pk>/", views.SeeRoomView.as_view()),
-]
+router = DefaultRouter()
+router.register("", viewset=viewsets.RoomViewset, basename="room")
+urlpatterns = router.urls
--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,4 @@
 from rest_framework.generics import ListAPIView, RetrieveAPIView
-from rest_framework.response import Response
 from .models import Room
 from .serializers import RoomSerializer, BigRoomSerializer
 
