61295d7d71efe6aedef3219f9252918bc35e7dfc
miranaky <miranaky@gmail.com>
2020-11-27 07:03:05 -0600

    Django REST API - change WriteRoomSerializer (Serializer -> ModelSerializer)

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -12,18 +12,14 @@ class ReadRoomSerializer(serializers.ModelSerializer):
         exclude = ("modified",)
 
 
-class WriteRoomSerializer(serializers.Serializer):
-    name = serializers.CharField(max_length=140)
-    address = serializers.CharField(max_length=140)
-    price = serializers.IntegerField()
-    beds = serializers.IntegerField(default=1)
-    lat = serializers.DecimalField(max_digits=10, decimal_places=6)
-    lng = serializers.DecimalField(max_digits=10, decimal_places=6)
-    bedrooms = serializers.IntegerField(default=1)
-    bathrooms = serializers.IntegerField(default=1)
-    check_in = serializers.TimeField(default="00:00:00")
-    check_out = serializers.TimeField(default="00:00:00")
-    instant_book = serializers.BooleanField(default=False)
+class WriteRoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        exclude = (
+            "user",
+            "modified",
+            "created",
+        )
 
     def validate(self, data):
         if self.instance:
@@ -35,21 +31,3 @@ class WriteRoomSerializer(serializers.Serializer):
         if check_in == check_out:
             raise serializers.ValidationError("Not enough time between changes")
         return data
-
-    def create(self, validated_data):
-        return Room.objects.create(**validated_data)
-
-    def update(self, instance, validate_data):
-        instance.name = validate_data.get("name", instance.name)
-        instance.address = validate_data.get("address", instance.address)
-        instance.price = validate_data.get("price", instance.price)
-        instance.beds = validate_data.get("beds", instance.beds)
-        instance.lat = validate_data.get("lat", instance.lat)
-        instance.lng = validate_data.get("lng", instance.lng)
-        instance.bedrooms = validate_data.get("bedrooms", instance.bedrooms)
-        instance.bathrooms = validate_data.get("bathrooms", instance.bathrooms)
-        instance.check_in = validate_data.get("check_in", instance.check_in)
-        instance.check_out = validate_data.get("check_out", instance.check_out)
-        instance.instant_book = validate_data.get("instant_book", instance.instant_book)
-        instance.save()
-        return instance
