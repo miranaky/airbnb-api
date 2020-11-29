7b736b8dd0cee5400013d15242fc34e1a897d62d
miranaky <miranaky@gmail.com>
2020-11-29 06:32:11 -0600

    Django REST API - user favs list , GET,PUT

--- users/views.py
+++ users/views.py
@@ -3,6 +3,8 @@ from rest_framework.decorators import api_view
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
+from rooms.serializers import RoomSerializer
+from rooms.models import Room
 from .serializers import ReadUserSerializer, WriteUserSerializer
 from .models import User
 
@@ -32,3 +34,29 @@ def user_detail(request, pk):
         return Response(ReadUserSerializer(user).data)
     except User.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+class FavViews(APIView):
+    permission_classes = [
+        IsAuthenticated,
+    ]
+
+    def get(self, request):
+        user = request.user
+        serializer = RoomSerializer(user.favs.all(), many=True).data
+        return Response(serializer)
+
+    def put(self, request):
+        pk = request.data.get("pk", None)
+        user = request.user
+        if pk is not None:
+            try:
+                room = Room.objects.get(pk=pk)
+                if room in user.favs.all():
+                    user.favs.remove(room)
+                else:
+                    user.favs.add(room)
+                return Response()
+            except Room.DoesNotExist:
+                pass
+        return Response(status=status.HTTP_400_BAD_REQUEST)
