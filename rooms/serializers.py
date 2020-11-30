9e606f233fe95c261bd55d70f332943a1287192a
miranaky <miranaky@gmail.com>
2020-11-30 05:26:55 -0600

    Django REST API - Bug fix when create room. you can't create user.

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -4,7 +4,7 @@ from .models import Room
 
 
 class RoomSerializer(serializers.ModelSerializer):
-    user = UserSerializer()
+    user = UserSerializer(read_only=True)
     is_fav = serializers.SerializerMethodField()
 
     class Meta:
@@ -34,3 +34,8 @@ class RoomSerializer(serializers.ModelSerializer):
             if user.is_authenticated:
                 return obj in user.favs.all()
         return False
+
+    def create(self, validatedata):
+        request = self.context.get("request")
+        room = Room.objects.create(**validatedata, user=request.user)
+        return room
