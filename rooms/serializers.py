19a7362258a19f122e3a3ed335166fef3ede9677
miranaky <miranaky@gmail.com>
2020-11-30 05:16:24 -0600

    Django REST API - RoomView change ViewSets from APIView

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -5,6 +5,7 @@ from .models import Room
 
 class RoomSerializer(serializers.ModelSerializer):
     user = UserSerializer()
+    is_fav = serializers.SerializerMethodField()
 
     class Meta:
         model = Room
@@ -25,3 +26,11 @@ class RoomSerializer(serializers.ModelSerializer):
         if check_in == check_out:
             raise serializers.ValidationError("Not enough time between changes")
         return data
+
+    def get_is_fav(self, obj):
+        request = self.context.get("request")
+        if request:
+            user = request.user
+            if user.is_authenticated:
+                return obj in user.favs.all()
+        return False
--- rooms/views.py
+++ rooms/views.py
@@ -1,77 +1,25 @@
-from rest_framework.views import APIView
+from rest_framework import permissions
+from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
-from rest_framework.pagination import PageNumberPagination
 from rest_framework.decorators import api_view
 from rest_framework import status
 from .models import Room
 from .serializers import RoomSerializer
+from .permissions import IsOwner
 
 
-class OwnPagination(PageNumberPagination):
-    page_size = 20
+class RoomViewSet(ModelViewSet):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
 
-
-class RoomsView(APIView):
-    def get(self, request):
-        paginator = OwnPagination()
-        rooms = Room.objects.all()
-        result = paginator.paginate_queryset(rooms, request)
-        serializer = RoomSerializer(result, many=True)
-        return paginator.get_paginated_response(serializer.data)
-
-    def post(self, request):
-        if not request.user.is_authenticated:
-            return Response(status=status.HTTP_401_UNAUTHORIZED)
-        serializer = RoomSerializer(data=request.data)
-        if serializer.is_valid():
-            room = serializer.save(user=request.user)
-            room_serializer = RoomSerializer(room).data
-            return Response(data=room_serializer, status=status.HTTP_200_OK)
+    def get_permissions(self):
+        if self.action == "list" or self.action == "retrieve":
+            permission_classes = [permissions.AllowAny]
+        elif self.action == "create":
+            permission_classes = [permissions.IsAuthenticated]
         else:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
-        return Response()
-
-
-class RoomView(APIView):
-    def get_room(self, pk):
-        try:
-            room = Room.objects.get(pk=pk)
-            return room
-        except Room.DoesNotExist:
-            return None
-
-    def get(self, request, pk):
-        room = self.get_room(pk)
-        if room is None:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-        else:
-            serializer = RoomSerializer(room).data
-            return Response(serializer)
-
-    def put(self, request, pk):
-        room = self.get_room(pk)
-        if room is None:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-        else:
-            if room.user != request.user:
-                return Response(status=status.HTTP_403_FORBIDDEN)
-            serializer = RoomSerializer(room, data=request.data, partial=True)
-            if serializer.is_valid():
-                room = serializer.save()
-                return Response(RoomSerializer(room).data)
-            else:
-                return Response(status=status.HTTP_400_BAD_REQUEST)
-            return Response()
-
-    def delete(self, request, pk):
-        room = self.get_room(pk)
-        if room is None:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-        else:
-            if room.user != request.user:
-                return Response(status=status.HTTP_403_FORBIDDEN)
-            room.delete()
-            return Response(status=status.HTTP_200_OK)
+            permission_classes = [IsOwner]
+        return [permission() for permission in permission_classes]
 
 
 @api_view(["GET"])
